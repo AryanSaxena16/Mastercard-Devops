2ae881b14e88f8f7aef0ff36eefe6b9d889da3d7
Tiruveedhula Chandra Mouli <ChandraMouli.T@mu-sigma.com>
2026-08-17 14:43:05 +0530

    Fix login crash

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("DevOps Git Demo");
         System.out.println("Login feature enabled");
+        System.out.println("Login crash fixed");
     }
 }
